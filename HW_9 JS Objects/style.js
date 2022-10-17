6f5e5fcac1428b5a24a3d17a085518b67d7bb8dc
Firsova Liubov <firsova.lyub@gmail.com>
2022-10-18 00:41:59 +0300

    done task 7

--- HW_9 JS Objects/style.js
+++ HW_9 JS Objects/style.js
@@ -50,26 +50,101 @@ alert(a);
 alert(b);
 alert(c);
 */
-
 // task 4
 
 /*
-
+let a;
+let b;
+let c;
+let length1;
+let length2;
+let close_length;
+let close_point;
+if (a > c && a < b) {
+    length1 = a - c; 
+    length2 = b - a;
+    if (length1 > length2){
+    close_length = length2;
+    close_point = b;
+    } else {
+    close_length = length1;
+    close_point = c;
+    }
+}
+if (b > a && b < c) {
+    close_length = b - a;
+    close_point = b;
+}
+if (c > b && c < a) {
+    close_length = a - c;
+    close_point = c;
+)
+alert(close_length);
+alert(close_point);
 */
-// task
+// task 5
 
 /*
-
+let x;
+let y;
+if (x == 0 && y == 0) {
+    alert(0);
+}
+if (x == 0 && y !== 0) {
+    alert(1);
+}
+if (x !== 0 && y == 0) {
+    alert(2);
+}
+if (x !== 0 && y !== 0){
+    alert(3);
+}
 */
-// task
+
+// task 6
 
 /*
-
+let x;
+let y;
+let quarter = 1;
+if (x < 0 && y > 0) {
+    quarter = 2;
+}
+if (x < 0 && y < 0) {
+    quarter = 3;
+}
+if (x > 0 && y < 0) {
+    quarter = 4;
+}
+alert(quarter);
 */
-// task
+
+// task 7
 
 /*
+let x1;
+let y1;
+let x2;
+let y2;
+let x3;
+let y3;
+let x4;
+let y4;
 
+if (x1 == x2) {
+    x4 == x3;
+}
+if (x1 !== x2) {
+    x4 == x1;
+}
+if (y1 == y2) {
+    y4 == y3;
+}
+if (y1 !== y2) {
+    y4 == y1;
+}
+alert(x4);
+alert(x5);
 */
 // task
 
