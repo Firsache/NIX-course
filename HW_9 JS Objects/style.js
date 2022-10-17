8905adf76656de08a5abc0c7f8c7462fb0ff2db3
Firsova Liubov <firsova.lyub@gmail.com>
2022-10-17 23:14:10 +0300

    done task 3

--- HW_9 JS Objects/style.js
+++ HW_9 JS Objects/style.js
@@ -1,20 +1,57 @@
 // task 1
 
 /*
-
+let i = 3;
+while (i) {
+    alert(i--); ------- виведе 3, 2, 1 і зупиниться, через те, умова while не виконується при 0 = false
+}
 */
 
-// task
+// task 2
 
 /*
-
+let a;
+let b;
+let c;
+if (a < b && b < c) {
+    a = a ** 2;
+    b = b ** 2;
+    c = c ** 2;
+} else {
+    a = -a;
+    b = -b;
+    c = -c;
+}
+alert(a);
+alert(b);
+alert(c);
 */
-// task
+// task 3
 
 /*
-
+let a;
+let b;
+let c;
+if (a < b && b < c) {
+    a = a ** 2;
+    b = b ** 2;
+    c = c ** 2;
+} else if (a > b && b > c) {
+    a = a ** 2;
+    b = b ** 2;
+    c = c ** 2;
+}
+else {
+    a = -a;
+    b = -b;
+    c = -c;
+}
+alert(a);
+alert(b);
+alert(c);
 */
-// task
+
+// task 4
 
 /*
 
