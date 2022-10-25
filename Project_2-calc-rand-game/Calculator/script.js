8f49d2ba5a33b4cb4874acc6574caca2bf1e6c95
Firsova Liubov <firsova.lyub@gmail.com>
2022-10-25 11:50:35 +0300

    created script. js, added primary btns and actions

---
+++ Project_2-calc-rand-game/Calculator/script.js
@@ -0,0 +1,22 @@
+let a = "";
+let b = "";
+let sign = "";
+let result = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['+', '-', 'X', '/'];
+//  my screen
+const out = document.querySelector('.calc-screen p');
+// button ac
+function clearAll() {
+    a = "";
+    b = "";
+    sign = "";
+    result = false;
+    out.textContent = 0;
+}
+document.querySelector('.ac').onclick = clearAll;
+document.querySelector('.buttons').onclick = (event) => {
+    if (!event.target.classList.contains('btn')) return; // we press on not a button
+    if (event.target.classList.contains('ac')) return; // we press on a button ac
+}
