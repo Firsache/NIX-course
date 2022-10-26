93e2701cbb2e7c40b6c6bc79ba3ac87f8de47de0
Firsova Liubov <firsova.lyub@gmail.com>
2022-10-26 14:23:23 +0300

    task 14 is done

--- HW_11 JS Map Set/script.js
+++ HW_11 JS Map Set/script.js
@@ -1,4 +1,4 @@
-// task 1 DONE
+// task 1
 
 /*
 function camelize(str) {
@@ -22,7 +22,8 @@ alert(camelize("-webkit-transition"));
 
 /*
 function filterRange(arr, a, b) {
-
+    let find = arr.filter(item => (item >= a && item <= b));
+    return find;
 }
 let arr = [5, 3, 8, 1];
 let filtered = filterRange(arr, 1, 4);
@@ -33,36 +34,45 @@ alert(arr); // 5,3,8,1 (без змін)
 
 /*
 function filterRangeInPlace(arr, a, b) {
-    
+    for (let i = 0; i < arr.length; i++) {
+        let elem = arr[i];
+        if (elem < a || elem > b) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
 }
-a ≤ arr[i] ≤ b
-
 let arr = [5, 3, 8, 1];
-filterRangeInPlace(arr, 1, 4); // видалені числа поза діапазоном 1..4
+filterRangeInPlace(arr, 1, 4);
 alert(arr ); // [3, 1]
 */
 // task 4
 
 /*
 let arr = [5, 2, 1, -10, 8];
-// ... ваш код для сортування за спаданням
-alert(arr); // 8, 5, 2, 1, -10
+arr.sort((a, b) => b - a);
+
+alert( arr ); // 8, 5, 2, 1, -10
 */
 // task 5
 
 /*
-functiion copySorted(arr), яка повертатиме таку копію.
+function copySorted(arr) {
+    return arr.slice().sort();
+}
 let arr = ["HTML", "JavaScript", "CSS"];
 let sorted = copySorted(arr);
 alert (sorted); // CSS, HTML, JavaScript
 alert(arr); // HTML, JavaScript, CSS (без змін)
 */
+
 // task 6
 
 /*
-Calculator
+наприкінці файлу
 */
-// task 7 DONE
+
+// task 7
 
 /*
 let vasya = { name: "Вася", age: 25 };
@@ -74,34 +84,114 @@ let users = [vasya, petya, masha, liubov];
 let names = users.map(item => item.name);
 alert(names);
 */
-// task 8
 
+// task 8
 /*
-function sortByAge(users) {
-    
+function sortByAge(users){
+    arr.sort((a, b) => a.age - b.age);
 }
+let vasya = {name : " Вася ", age: 25};
+let petya = {name : " Петя ", age: 30};
+let masha = {name : " Маша ", age: 28};
+let arr = [vasya, petya, masha];
+
+sortByAge(arr);
+// тепер : [ vasya , masha , petya ]
+alert( arr [0].name); // Вася
+alert ( arr [1].name); // Маша
+alert ( arr [2].name); // Петро
 */
 // task 9
 
 /*
+function getAverageAge(users) {
+    return users.reduce((previousValue, user) => previousValue + user.age, 0) / users.length;
+}
+
+let vasya = { name: "Вася", age: 25};
+let petya = { name: "Петя", age: 30};
+let masha = { name: "Маша", age: 29};
+let arr = [vasya, petya, masha];
+
+alert( getAverageAge(arr) );  // (25 + 30 + 29) / 3 = 28
 */
 // task 10
 
 /*
+function unique(arr) { 
+    let result = [];
+    for (let elem of arr) {
+        if (!result.includes(elem)) {
+            result.push(elem);
+        }
+    }
+    return result;
+}
+let strings = ["кришна", "кришна", "харе", "харе",
+"харе", "харе", "кришна", "крішна", ":-O"];
+alert (unique (strings)); // кришна, харе, :-O
 */
 // task 11
 
 /*
+function unique(arr) { 
+    return [... new Set(arr)];
+}
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+"Krishna", "Krishna", "Hare", "Hare", ":-O"];
+alert(unique(values)); // Hare,Krishna,:-O
 */
 // task 12
 
 /*
+function makeCounter() {
+    let count = 0;
+    return function() {
+        return count++;
+    };
+}
+let counter = makeCounter();
+let counter2 = makeCounter();
+alert (counter ()); // 0
+alert (counter ()); // 1
+alert (counter2 ()); // покаже знову 0
+alert(counter2()); // покаже знову 1
+---------------------------------------
+count  є локальною змінною функції makeCounter, мы не можемо ззовні її змінити тощо.
+коли ми визиваємо функцїю makeCounter створюється нове незалежне лексичне оточення
+counter та counter2 прив'язані до різних викликів функції makeCounter
 */
+
 // task 13
 
 /*
+function Counter() {
+    let count = 0;
+    this.up = function() {
+    return ++count;
+    };
+    this.down = function() {
+return - count;
+    };
+}
+let counter = new Counter();
+alert(counter.up()); //1
+alert(counter.up()); //2 ----- this.up та this.down мають одну спільну змінну count та вони належать до одного лексимчного оточення Counter
+alert(counter.down() ); //1
 */
+
 // task 14
 
 /*
+function factorial(n) {
+  return (n != 1) ? n * factorial(n - 1) : 1;
+}
+
+alert( factorial(5) ); // 120
+*/
+
+// task 6
+
+/*
+Calculator
 */
