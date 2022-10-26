a95f32d54f7a6e8c1540347b7afdc2b55e727f29
Firsova Liubov <firsova.lyub@gmail.com>
2022-10-26 12:22:25 +0300

    done task one and seven

---
+++ HW_11 JS Map Set/script.js
@@ -0,0 +1,107 @@
+// task 1 DONE
+
+/*
+function camelize(str) {
+    let arr = str.split('-');
+    let newArr = [];
+    
+    for (let elem of arr) {
+        if (elem == '') continue;
+        let newElem = elem[0].toUpperCase() + elem.slice(1);
+        newArr.push(newElem);
+    }
+    let newStr = newArr.join('');
+    return newStr[0].toLowerCase() + newStr.slice(1);
+}
+alert(camelize("background-color"));
+alert(camelize("list-style-image"));
+alert(camelize("-webkit-transition"));
+
+*/
+// task 2
+
+/*
+function filterRange(arr, a, b) {
+
+}
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+alert(filtered); // 3,1 (збігаються значення)
+alert(arr); // 5,3,8,1 (без змін)
+*/
+// task 3
+
+/*
+function filterRangeInPlace(arr, a, b) {
+    
+}
+a ≤ arr[i] ≤ b
+
+let arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 1, 4); // видалені числа поза діапазоном 1..4
+alert(arr ); // [3, 1]
+*/
+// task 4
+
+/*
+let arr = [5, 2, 1, -10, 8];
+// ... ваш код для сортування за спаданням
+alert(arr); // 8, 5, 2, 1, -10
+*/
+// task 5
+
+/*
+functiion copySorted(arr), яка повертатиме таку копію.
+let arr = ["HTML", "JavaScript", "CSS"];
+let sorted = copySorted(arr);
+alert (sorted); // CSS, HTML, JavaScript
+alert(arr); // HTML, JavaScript, CSS (без змін)
+*/
+// task 6
+
+/*
+Calculator
+*/
+// task 7 DONE
+
+/*
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+let liubov = { name: "Любов", age: 27 };
+
+let users = [vasya, petya, masha, liubov];
+let names = users.map(item => item.name);
+alert(names);
+*/
+// task 8
+
+/*
+function sortByAge(users) {
+    
+}
+*/
+// task 9
+
+/*
+*/
+// task 10
+
+/*
+*/
+// task 11
+
+/*
+*/
+// task 12
+
+/*
+*/
+// task 13
+
+/*
+*/
+// task 14
+
+/*
+*/
